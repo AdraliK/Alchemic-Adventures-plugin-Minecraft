0e00d3e282b98a76bb65304e05c7e0259f59e6d8
AdraliK <adralik@gmail.com>
2025-01-13 21:50:43 +0300

    feat MIN-33: доработан камнерез

--- src/main/java/listeners/StonecutterInteract.java
+++ src/main/java/listeners/StonecutterInteract.java
@@ -1,10 +1,15 @@
 package listeners;
 
 import adralik.alchemicAndAdventures.Main;
+import io.papermc.paper.event.entity.EntityMoveEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -12,26 +17,49 @@ import org.bukkit.scheduler.BukkitRunnable;
 public class StonecutterInteract implements Listener {
 
     @EventHandler
-    public void onPlayerMove(PlayerMoveEvent event) {
-        Player player = event.getPlayer();
-        boolean isOnStonecutter = player.getLocation().getBlock().getType() == Material.STONECUTTER;
+    public void onEntityMove(EntityMoveEvent e) {
+        Entity entity = e.getEntity();
+        if (!(entity instanceof LivingEntity livingEntity)) {
+            return;
+        }
+        damageEntityFromStonecutter(livingEntity);
+    }
 
-        if (isOnStonecutter && !player.hasMetadata("onStonecutter")) {
-            player.setMetadata("onStonecutter", new FixedMetadataValue(Main.javaPlugin, true));
-            player.damage(1.0); // Моментальный урон
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent e) {
+        Player player = e.getPlayer();
+        damageEntityFromStonecutter(player);
+    }
+
+    private static void damageEntityFromStonecutter(LivingEntity livingEntity) {
+        boolean isOnStonecutter = livingEntity.getLocation().getBlock().getType() == Material.STONECUTTER;
+
+        if (isOnStonecutter && !livingEntity.hasMetadata("onStonecutter")) {
+            livingEntity.setMetadata("onStonecutter", new FixedMetadataValue(Main.javaPlugin, true));
+            livingEntity.damage(1.0); // Моментальный урон
 
             new BukkitRunnable() {
                 @Override
                 public void run() {
-                    if (!player.hasMetadata("onStonecutter")) {
+                    if (!livingEntity.hasMetadata("onStonecutter")) {
                         cancel();
                         return;
                     }
-                    player.damage(1.0);
+                    livingEntity.damage(1.0);
                 }
             }.runTaskTimer(Main.javaPlugin, 20L, 20L); // Урон каждую секунду
-        } else if (!isOnStonecutter && player.hasMetadata("onStonecutter")) {
+        } else if (!isOnStonecutter && livingEntity.hasMetadata("onStonecutter")) {
+            livingEntity.removeMetadata("onStonecutter", Main.javaPlugin);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+
+        if (player.getLocation().getBlock().getType() == Material.STONECUTTER) {
             player.removeMetadata("onStonecutter", Main.javaPlugin);
+            event.setDeathMessage(player.getName() + " был безжалостно разрезан камнерезом!");
         }
     }
 }
