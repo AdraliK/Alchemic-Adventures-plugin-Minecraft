947a1040c2cf0ddb2413ef7378ec62e99f24a08e
AdraliK <adralik@gmail.com>
2025-02-21 21:29:06 +0300

    fix MIN-93: Испрвлена логика починки наковальни

--- src/main/java/listeners/AnvilInteract.java
+++ src/main/java/listeners/AnvilInteract.java
@@ -7,7 +7,6 @@ import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Directional;
 import org.bukkit.event.Listener;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -17,14 +16,13 @@ public class AnvilInteract implements Listener {
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent e) {
-        if (e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        if (!(e.getAction() == Action.RIGHT_CLICK_BLOCK && e.getPlayer().isSneaking())) return;
         if (e.getClickedBlock() == null || e.getItem() == null) return;
 
         Block block = e.getClickedBlock();
         ItemStack itemInHand = e.getItem();
 
-        // Проверяем, что блок - наковальня
-        if (block.getType() == Material.ANVIL || block.getType() == Material.CHIPPED_ANVIL || block.getType() == Material.DAMAGED_ANVIL) {
+        if (block.getType() == Material.CHIPPED_ANVIL || block.getType() == Material.DAMAGED_ANVIL) {
 
             if (itemInHand.getType() != Material.IRON_INGOT) return;
             if (itemInHand.getAmount() < 3) {
@@ -33,14 +31,12 @@ public class AnvilInteract implements Listener {
                 return;
             }
 
-            // Уменьшаем количество железных слитков на 3
             itemInHand.setAmount(itemInHand.getAmount() - 3);
 
             BlockData originalData = block.getBlockData();
 
             if (originalData instanceof Directional directional) {
 
-                // Чиним наковальню, повышая её градацию
                 switch (block.getType()) {
                     case DAMAGED_ANVIL:
                         block.setType(Material.CHIPPED_ANVIL);
@@ -57,7 +53,6 @@ public class AnvilInteract implements Listener {
                     ((Directional) newData).setFacing(directional.getFacing());
                     block.setBlockData(newData);
                 }
-                // Воспроизводим звук починки в мире
                 block.getWorld().playSound(block.getLocation(), Sound.BLOCK_ANVIL_USE, SoundCategory.BLOCKS, 1.0f, 2.0f);
                 e.setCancelled(true);
             }
