b5bf3ac7dd66cbb60e6e4eee7a77ba3d241864f3
AdraliK <adralik@gmail.com>
2025-02-25 21:56:23 +0300

    Fix MIN-99: Добавлено сообщение, что можно починить наковальню

--- src/main/java/listeners/AnvilInteract.java
+++ src/main/java/listeners/AnvilInteract.java
@@ -6,10 +6,12 @@ import org.bukkit.SoundCategory;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Directional;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class AnvilInteract implements Listener {
@@ -56,6 +58,20 @@ public class AnvilInteract implements Listener {
                 block.getWorld().playSound(block.getLocation(), Sound.BLOCK_ANVIL_USE, SoundCategory.BLOCKS, 1.0f, 2.0f);
                 e.setCancelled(true);
             }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerLookAtAnvil(PlayerToggleSneakEvent event) {
+        Player player = event.getPlayer();
+        Block targetBlock = player.getTargetBlockExact(5);
+
+        if (event.isSneaking() &&
+                targetBlock != null &&
+                targetBlock.getType().name().contains("_ANVIL") &&
+                player.getInventory().getItemInMainHand().getType() == Material.IRON_INGOT) {
+
+            player.sendActionBar("Нажмите ПКМ, чтобы починить наковальню");
         }
     }
 }
