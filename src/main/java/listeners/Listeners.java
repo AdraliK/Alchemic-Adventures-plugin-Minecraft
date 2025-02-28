1d07258b91910462b10a98b084db114c138bf7b4
AdraliK <adralik@gmail.com>
2025-02-28 16:01:46 +0300

    fix MIN-126: Сохраняется лор голов при добычи

--- src/main/java/listeners/Listeners.java
+++ src/main/java/listeners/Listeners.java
@@ -5,6 +5,7 @@ import listeners.blocks.AnvilInteract;
 import listeners.blocks.CauldronInteract;
 import listeners.blocks.ComposterInteract;
 import listeners.blocks.StonecutterInteract;
+import listeners.items.CustomHeadDrop;
 import listeners.items.CustomMinecart;
 import listeners.items.SoupFix;
 import listeners.items.TotemCooldown;
@@ -18,21 +19,26 @@ public class Listeners {
     public static void init(){
         pluginManager = Bukkit.getPluginManager();
 
+    //------------------BLOCKS-------------------//
+
         pluginManager.registerEvents(new CauldronInteract(), Main.javaPlugin);
         pluginManager.registerEvents(new ComposterInteract(), Main.javaPlugin);
         pluginManager.registerEvents(new AnvilInteract(), Main.javaPlugin);
         pluginManager.registerEvents(new StonecutterInteract(), Main.javaPlugin);
 
+    //------------------ITEMS--------------------//
+
         pluginManager.registerEvents(new SoupFix(), Main.javaPlugin);
+        pluginManager.registerEvents(new TotemCooldown(), Main.javaPlugin);
+        pluginManager.registerEvents(new CustomMinecart(), Main.javaPlugin);
+        pluginManager.registerEvents(new CustomHeadDrop(), Main.javaPlugin);
+
+    //------------------WORLD--------------------//
 
         pluginManager.registerEvents(new MoonPhaseChecker(), Main.javaPlugin);
         pluginManager.registerEvents(new FullMoonMobsBuffs(), Main.javaPlugin);
         pluginManager.registerEvents(new PaleGarden(), Main.javaPlugin);
-
-        pluginManager.registerEvents(new TotemCooldown(), Main.javaPlugin);
         pluginManager.registerEvents(new FarmLandProtection(), Main.javaPlugin);
-
         pluginManager.registerEvents(new Explosions(), Main.javaPlugin);
-        pluginManager.registerEvents(new CustomMinecart(), Main.javaPlugin);
     }
 }
