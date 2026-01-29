9052a216f20dde3309d75d7a9553333d08d70289
AdraliK <adralik@gmail.com>
2026-01-29 23:20:20 +0300

    feat MIN-345: многострочные сообщения /hints

--- src/main/java/commands/HintsCommand.java
+++ src/main/java/commands/HintsCommand.java
@@ -6,15 +6,17 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.List;
+
 import static adralik.vanillaPlus.Main.config;
 
 public class HintsCommand implements CommandExecutor {
 
     private static final String configPath = "hints";
-    private static final String hintsEnabledMessage =
-            config.getString(configPath + ".hints-enabled-message", "Подсказки включены");
-    private static final String hintsDisabledMessage =
-            config.getString(configPath + ".hints-disabled-message", "Подсказки выключены");
+    private static final List<String> hintsEnabledMessage =
+            config.getStringList(configPath + ".hints-enabled-message");
+    private static final List<String> hintsDisabledMessage =
+            config.getStringList(configPath + ".hints-disabled-message");
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -26,10 +28,10 @@ public class HintsCommand implements CommandExecutor {
 
         if (HintsManager.hasHints(player.getUniqueId())) {
             HintsManager.disableHints(player.getUniqueId());
-            player.sendMessage(hintsDisabledMessage);
+            hintsDisabledMessage.forEach(player::sendMessage);
         } else {
             HintsManager.enableHints(player.getUniqueId());
-            player.sendMessage(hintsEnabledMessage);
+            hintsEnabledMessage.forEach(player::sendMessage);
         }
 
         return true;
